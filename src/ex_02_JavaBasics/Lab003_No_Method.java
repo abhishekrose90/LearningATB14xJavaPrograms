92e139f1f6d36f0dbfc310c1f69ed66a5049f8fb
abhishek rose <abhishekrose90@gmail.com>
2025-09-21 17:11:21 +0530

    second push

---
+++ src/ex_02_JavaBasics/Lab003_No_Method.java
@@ -0,0 +1,4 @@
+package ex_02_JavaBasics;
+
+public class Lab003_No_Method {
+}
---
+++ src/ex_02_JavaBasics/Lab004_Comments.java
@@ -0,0 +1,10 @@
+package ex_02_JavaBasics;
+
+public class Lab004_Comments {
+    public static void main(String[] args) {
+//        this is comment (for single line)[shortcut key - ctrl+/]
+        /**this is second comment (for multiple line comment)[ctrl+shift+/]
+                 this is third comment**/
+        System.out.printf("Hello World");
+    }
+}
